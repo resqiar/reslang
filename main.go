eac5a5c83636e493814efecadd81deaf3fd416f4
resqiar <resqiageng@gmail.com>
2023-08-30 02:20:49 +0700

    add a fun rough malloc calculations

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"reslang/lexer"
+	"runtime"
 )
 
 func main() {
@@ -13,10 +14,19 @@ func main() {
 	let two = 2;
 
 	let sum = fn(a, b) {
-		a + b;
+		return a + b;
 	};
 
 	let result = sum(one, two);
+
+	!-/*5;
+	5 < 10 > 5;
+
+	if (result < 10) {
+		result = sum(result * 100);
+	};
+
+	let foreverFalse = false;
 	`
 
 	var l *lexer.Lexer = lexer.New(code)
@@ -28,8 +38,14 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var mems = make([]int64, 0)
+
 	for {
+		var pointA runtime.MemStats
+		runtime.ReadMemStats(&pointA)
 		token := l.Parse()
+		var pointB runtime.MemStats
+		runtime.ReadMemStats(&pointB)
 
 		_, err := file.Write([]byte(fmt.Sprintf("{%v %v}\n", token.Type, token.Literal)))
 		if err != nil {
@@ -39,5 +55,15 @@ func main() {
 		if token.Type == "EOF" {
 			break
 		}
+
+		mems = append(mems, int64(pointB.TotalAlloc-pointA.TotalAlloc))
 	}
+
+	var sum int64
+	for _, v := range mems {
+		sum += v
+	}
+
+	log.Println(sum, "bytes total allocated memory")
+	log.Println(sum/int64(len(mems)), "bytes avg allocated memory per iteration")
 }
